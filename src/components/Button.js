ea98783ab78b2d9584b0e7bef6e10d9e91c93e63
msobczyk-x <m.sobczyk55@wp.pl>
2022-02-09 20:01:11 +0100

    fixed function

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ import "./Button.css";
 
 const Button = ({ className, value, onClick }) => {
     return (
-        <button className={className} onClick={onclick}>
+        <button className={className} onClick={onClick}>
             {value}
         </button>
     );
